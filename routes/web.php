178d8fa093ce2779c6902f323cc3c70e49f9942d
Luiz Fellipe <luiz.fellipe@scandiweb.com>
2023-08-25 12:38:53 -0300

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// All listings 
+Route::get('/listings', function () {
+    return view(
+        'listings',
+        [
+            'listings' => Listing::all()
+        ]
+    );
 });
+
+// single listing
+Route::get('/listings/{listing}', function (Listing $listing) {
+    return view('listing', [
+        'listing' => $listing
+    ]); 
+});
+
+/* Route::get('/hello', function () {
+    return response('<h1>hello world</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    dd($request->name . ' ' . $request->city);
+});
+*/
